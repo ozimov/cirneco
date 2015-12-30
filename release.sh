3249fe1e8d1d357a66bba14a22cd81dd3ff3fc2d
Roberto Trunfio <roberto.trunfio@gmail.com>
2015-12-30 16:16:55 +0100

    Preparing deploy release 0.1

--- release.sh
+++ release.sh
@@ -11,8 +11,8 @@ echo "########################################"
 echo "########################################"
 echo "When at prompt, type the release version (e.g. from 1.0-SNAPSHOT to 1.0)\n"
 mvn --settings ~/.m2/home-settings.xml versions:set
-git add pom.xml */pom.xml
-git commit -m "[Deploy phase] Preparing release"
+#git add pom.xml */pom.xml
+git commit -am "[Deploy phase] Preparing release"
 mvn --settings ~/.m2/home-settings.xml clean deploy -P release
 mvn --settings ~/.m2/home-settings.xml scm:tag
 
@@ -21,8 +21,8 @@ echo "########################################"
 echo "########################################"
 echo "When at prompt, type the SNAPSHOT version (e.g. from 1.0 to 2.0-SNAPSHOT)\n"
 mvn --settings ~/.m2/home-settings.xml versions:set
-git add pom.xml */pom.xml
-git commit -m "[Deploy phase] Preparing for next iteration"
+#git add pom.xml */pom.xml
+git commit -am "[Deploy phase] Preparing for next iteration"
 
 # updating origin
 git push --force --follow-tags
