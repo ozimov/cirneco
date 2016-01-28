bd4139d98204fbad36fa7f2a603ae0c467f5bd25
Roberto Trunfio <roberto.trunfio@zalando.de>
2016-01-28 20:37:14 +0100

    Added test for cluent assertion

---
+++ hamcrest-matchers/java7-hamcrest-matchers/src/test/java/it/ozimov/cirneco/hamcrest/java7/AssertFluentlyTest.java
@@ -0,0 +1,154 @@
+package it.ozimov.cirneco.hamcrest.java7;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.junit.rules.ExpectedException;
+
+public class AssertFluentlyTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void testGivenWithReasonAssertThatSuccess() throws Exception {
+
+        // Arrange
+        final String actual = "Test";
+        final String expected = "Test";
+
+        // Act+Assert
+        AssertFluently.given(actual).withReason("Unit test").assertThat(is(expected));
+
+    }
+
+    @Test
+    public void testGivenWithReasonAssertThatFailure() throws Exception {
+
+        // Arrange
+        expectedException.expect(AssertionError.class);
+        expectedException.expectMessage("Unit test");
+
+        final String actual = "Test";
+        final String expected = "Not Test";
+
+        // Act+Assert
+        AssertFluently.given(actual).withReason("Unit test").assertThat(is(expected));
+
+        fail("Should throw an exception");
+    }
+
+    @Test
+    public void testGivenWithReasonAssertIsSuccess() throws Exception {
+
+        // Arrange
+        final String actual = "Test";
+        final String expected = "Test";
+
+        // Act+Assert
+        AssertFluently.given(actual).withReason("Unit test").assertIs(equalTo(expected));
+
+    }
+
+    @Test
+    public void testGivenWithReasonAssertIsFailure() throws Exception {
+
+        // Arrange
+        expectedException.expect(AssertionError.class);
+        expectedException.expectMessage("Unit test");
+
+        final String actual = "Test";
+        final String expected = "Not Test";
+
+        // Act+Assert
+        AssertFluently.given(actual).withReason("Unit test").assertIs(equalTo(expected));
+
+        fail("Should throw an exception");
+    }
+
+    @Test
+    public void testWithReasonAssertThatSuccess() throws Exception {
+
+        // Act+Assert
+        AssertFluently.withReason("Unit test").assertThat(true);
+    }
+
+    @Test
+    public void testWithReasonAssertThatFailure() throws Exception {
+
+        // Arrange
+        expectedException.expect(AssertionError.class);
+        expectedException.expectMessage("Unit test");
+
+        // Act+Assert
+        AssertFluently.withReason("Unit test").assertThat(false);
+
+        fail("Should throw an exception");
+    }
+
+    @Test
+    public void testWithReasonAssertIsSuccess() throws Exception {
+
+        // Act+Assert
+        AssertFluently.withReason("Unit test").assertIs(true);
+
+    }
+
+    @Test
+    public void testWithReasonAssertIsFailure() throws Exception {
+
+        // Arrange
+        expectedException.expect(AssertionError.class);
+        expectedException.expectMessage("Unit test");
+
+        // Act+Assert
+        AssertFluently.withReason("Unit test").assertIs(false);
+
+        fail("Should throw an exception");
+    }
+
+    @Test
+    public void testAssertThatSuccess() throws Exception {
+
+        // Act+Assert
+        AssertFluently.assertThat(true);
+    }
+
+    @Test
+    public void testAssertThatFailure() throws Exception {
+
+        // Arrange
+        expectedException.expect(AssertionError.class);
+
+        // Act+Assert
+        AssertFluently.assertThat(false);
+
+        fail("Should throw an exception");
+    }
+
+    @Test
+    public void testAssertIsSuccess() throws Exception {
+
+        // Act+Assert
+        AssertFluently.assertIs(true);
+
+    }
+
+    @Test
+    public void testAssertIsFailure() throws Exception {
+
+        // Arrange
+        expectedException.expect(AssertionError.class);
+
+        // Act+Assert
+        AssertFluently.assertIs(false);
+
+        fail("Should throw an exception");
+    }
+
+}
