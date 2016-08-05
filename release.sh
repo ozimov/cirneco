ff89dba3de31f137059012c22692a2826223726f
Trunfio Roberto <trunfio@NB-AMBP-139.local>
2016-08-05 15:07:01 +0200

    [Deploy phase] Preparing for next iteration

--- release.sh
+++ release.sh
@@ -51,7 +51,7 @@ if [ -d "target/site/apidocs/" ]; then
     git fetch --depth=1 javadoc gh-pages
     git add --all
     git commit -m "Added docs for current release"
-    git merge --no-edit -s ours remotes/javadoc/gh-pages
+    git merge --allow-unrelated-histories --no-edit -s ours remotes/javadoc/gh-pages
     git push javadoc master:gh-pages
     rm -r -f .git
     cd ../../..
